013fd33eb04c8bac8b2020f9c5eed203ea6c3da9
TyurinaNataliya <TyurinaNataliya@gmail.com>
2024-01-19 17:11:28 +0300

    Валидация

--- src/components/modals-components/modal-add-review.tsx
+++ src/components/modals-components/modal-add-review.tsx
@@ -3,6 +3,12 @@ import { TypeProductReview } from '../../type-data/type';
 import { useAppDispatch } from '../../hooks/store';
 import { postReviewProduct } from '../../services/thunk/post-review-product';
 import { fetchReviewsProductAction } from '../../services/thunk/fetch-reviews-product';
+import {
+  MAX_COUNT_LETTERS_INPUT,
+  MAX_COUNT_LETTERS_NAME,
+  MIN_COUNT_LETTERS_INPUT,
+  MIN_COUNT_LETTERS_NAME,
+} from '../../const';
 
 type Props = {
   handleCloseModalReview: () => void;
@@ -65,6 +71,10 @@ function ModalAddReview({
     handleCloseModalReview();
   }
 
+  const isInvalidInputs = (nameInput: string) =>
+    nameInput.length < MIN_COUNT_LETTERS_INPUT ||
+    nameInput.length > MAX_COUNT_LETTERS_INPUT;
+
   return (
     <div className="modal is-active">
       <div className="modal__wrapper">
@@ -105,106 +115,237 @@ function ModalAddReview({
                         ))}
                     </div>
                     <div className="rate__progress">
-                      <span className="rate__stars">{ratingStars}</span>
+                      <span className="rate__stars">
+                        {ratingStars ? ratingStars : 0}
+                      </span>
                       <span>/</span>
                       <span className="rate__all-stars">5</span>
                     </div>
                   </div>
                   <p className="rate__message">Нужно оценить товар</p>
                 </fieldset>
-                <div className="custom-input form-review__item">
-                  <label>
-                    <span className="custom-input__label">
-                      Ваше имя
-                      <svg width="9" height="9" aria-hidden="true">
-                        <use xlinkHref="#icon-snowflake"></use>
-                      </svg>
-                    </span>
-                    <input
-                      type="text"
-                      name="user-name"
-                      minLength={2}
-                      maxLength={15}
-                      onChange={nameChangeHandle}
-                      value={reviewName}
-                      placeholder="Введите ваше имя"
-                      required
-                    />
-                  </label>
-                  <p className="custom-input__error">Нужно указать имя</p>
-                </div>
-                <div className="custom-input form-review__item">
-                  <label>
-                    <span className="custom-input__label">
-                      Достоинства
-                      <svg width="9" height="9" aria-hidden="true">
-                        <use xlinkHref="#icon-snowflake"></use>
-                      </svg>
-                    </span>
-                    <input
-                      type="text"
-                      name="user-plus"
-                      minLength={10}
-                      maxLength={160}
-                      onChange={advantagesChangeHandle}
-                      value={reviewAdvantages}
-                      placeholder="Основные преимущества товара"
-                      required
-                    />
-                  </label>
-                  <p className="custom-input__error">
-                    Нужно указать достоинства
-                  </p>
-                </div>
-                <div className="custom-input form-review__item">
-                  <label>
-                    <span className="custom-input__label">
-                      Недостатки
-                      <svg width="9" height="9" aria-hidden="true">
-                        <use xlinkHref="#icon-snowflake"></use>
-                      </svg>
-                    </span>
-                    <input
-                      type="text"
-                      name="user-minus"
-                      minLength={10}
-                      maxLength={160}
-                      onChange={disadvantagesChangeHandle}
-                      value={reviewDisadvantages}
-                      placeholder="Главные недостатки товара"
-                      required
-                    />
-                  </label>
-                  <p className="custom-input__error">
-                    Нужно указать недостатки
-                  </p>
-                </div>
-                <div className="custom-textarea form-review__item">
-                  <label>
-                    <span className="custom-textarea__label">
-                      Комментарий
-                      <svg width="9" height="9" aria-hidden="true">
-                        <use xlinkHref="#icon-snowflake"></use>
-                      </svg>
-                    </span>
-                    <textarea
-                      name="user-comment"
-                      minLength={10}
-                      maxLength={160}
-                      onChange={commentaryChangeHandle}
-                      value={reviewCommentary}
-                      placeholder="Поделитесь своим опытом покупки"
-                      required
-                    />
-                  </label>
-                  <div className="custom-textarea__error">
-                    Нужно добавить комментарий
+                {reviewName === '' ? (
+                  <div className="custom-input form-review__item">
+                    <label>
+                      <span className="custom-input__label">
+                        Ваше имя
+                        <svg width="9" height="9" aria-hidden="true">
+                          <use xlinkHref="#icon-snowflake"></use>
+                        </svg>
+                      </span>
+                      <input
+                        type="text"
+                        name="user-name"
+                        minLength={MIN_COUNT_LETTERS_NAME}
+                        maxLength={MAX_COUNT_LETTERS_NAME}
+                        onChange={nameChangeHandle}
+                        value={reviewName}
+                        placeholder="Введите ваше имя"
+                        required
+                      />
+                    </label>
+                    <p className="custom-input__error">Нужно указать имя</p>
                   </div>
-                </div>
+                ) : (
+                  <div
+                    className={
+                      (reviewName &&
+                        reviewName.length < MIN_COUNT_LETTERS_NAME) ||
+                      reviewName.length > MAX_COUNT_LETTERS_NAME
+                        ? 'custom-input form-review__item is-invalid input'
+                        : 'custom-input form-review__item is-valid input'
+                    }
+                  >
+                    <label>
+                      <span className="custom-input__label">
+                        Ваше имя
+                        <svg width="9" height="9" aria-hidden="true">
+                          <use xlinkHref="#icon-snowflake"></use>
+                        </svg>
+                      </span>
+                      <input
+                        type="text"
+                        name="user-name"
+                        minLength={MIN_COUNT_LETTERS_NAME}
+                        maxLength={MAX_COUNT_LETTERS_NAME}
+                        onChange={nameChangeHandle}
+                        value={reviewName}
+                        placeholder="Введите ваше имя"
+                        required
+                      />
+                    </label>
+                    <p className="custom-input__error">Нужно указать имя</p>
+                  </div>
+                )}
+
+                {reviewAdvantages === '' ? (
+                  <div className="custom-input form-review__item">
+                    <label>
+                      <span className="custom-input__label">
+                        Достоинства
+                        <svg width="9" height="9" aria-hidden="true">
+                          <use xlinkHref="#icon-snowflake"></use>
+                        </svg>
+                      </span>
+                      <input
+                        type="text"
+                        name="user-plus"
+                        minLength={MIN_COUNT_LETTERS_INPUT}
+                        maxLength={MAX_COUNT_LETTERS_INPUT}
+                        onChange={advantagesChangeHandle}
+                        value={reviewAdvantages}
+                        placeholder="Основные преимущества товара"
+                        required
+                      />
+                    </label>
+                    <p className="custom-input__error">
+                      Нужно указать достоинства
+                    </p>
+                  </div>
+                ) : (
+                  <div
+                    className={
+                      isInvalidInputs(reviewAdvantages)
+                        ? 'custom-input form-review__item is-invalid input'
+                        : 'custom-input form-review__item is-valid input'
+                    }
+                  >
+                    <label>
+                      <span className="custom-input__label">
+                        Достоинства
+                        <svg width="9" height="9" aria-hidden="true">
+                          <use xlinkHref="#icon-snowflake"></use>
+                        </svg>
+                      </span>
+                      <input
+                        type="text"
+                        name="user-plus"
+                        minLength={MIN_COUNT_LETTERS_INPUT}
+                        maxLength={MAX_COUNT_LETTERS_INPUT}
+                        onChange={advantagesChangeHandle}
+                        value={reviewAdvantages}
+                        placeholder="Основные преимущества товара"
+                        required
+                      />
+                    </label>
+                    <p className="custom-input__error">
+                      Нужно указать достоинства
+                    </p>
+                  </div>
+                )}
+                {reviewDisadvantages === '' ? (
+                  <div className="custom-input form-review__item">
+                    <label>
+                      <span className="custom-input__label">
+                        Недостатки
+                        <svg width="9" height="9" aria-hidden="true">
+                          <use xlinkHref="#icon-snowflake"></use>
+                        </svg>
+                      </span>
+                      <input
+                        type="text"
+                        name="user-minus"
+                        minLength={MIN_COUNT_LETTERS_INPUT}
+                        maxLength={MAX_COUNT_LETTERS_INPUT}
+                        onChange={disadvantagesChangeHandle}
+                        value={reviewDisadvantages}
+                        placeholder="Главные недостатки товара"
+                        required
+                      />
+                    </label>
+                    <p className="custom-input__error">
+                      Нужно указать недостатки
+                    </p>
+                  </div>
+                ) : (
+                  <div
+                    className={
+                      isInvalidInputs(reviewDisadvantages)
+                        ? 'custom-input form-review__item is-invalid input'
+                        : 'custom-input form-review__item is-valid input'
+                    }
+                  >
+                    <label>
+                      <span className="custom-input__label">
+                        Недостатки
+                        <svg width="9" height="9" aria-hidden="true">
+                          <use xlinkHref="#icon-snowflake"></use>
+                        </svg>
+                      </span>
+                      <input
+                        type="text"
+                        name="user-minus"
+                        minLength={MIN_COUNT_LETTERS_INPUT}
+                        maxLength={MAX_COUNT_LETTERS_INPUT}
+                        onChange={disadvantagesChangeHandle}
+                        value={reviewDisadvantages}
+                        placeholder="Главные недостатки товара"
+                        required
+                      />
+                    </label>
+                    <p className="custom-input__error">
+                      Нужно указать недостатки
+                    </p>
+                  </div>
+                )}
+                {reviewCommentary === '' ? (
+                  <div className="custom-textarea form-review__item">
+                    <label>
+                      <span className="custom-textarea__label">
+                        Комментарий
+                        <svg width="9" height="9" aria-hidden="true">
+                          <use xlinkHref="#icon-snowflake"></use>
+                        </svg>
+                      </span>
+                      <textarea
+                        name="user-comment"
+                        minLength={MIN_COUNT_LETTERS_INPUT}
+                        maxLength={MAX_COUNT_LETTERS_INPUT}
+                        onChange={commentaryChangeHandle}
+                        value={reviewCommentary}
+                        placeholder="Поделитесь своим опытом покупки"
+                        required
+                      />
+                    </label>
+                    <div className="custom-textarea__error">
+                      Нужно добавить комментарий
+                    </div>
+                  </div>
+                ) : (
+                  <div
+                    className={
+                      isInvalidInputs(reviewCommentary)
+                        ? 'custom-textarea form-review__item is-invalid input'
+                        : 'custom-textarea form-review__item is-valid input'
+                    }
+                  >
+                    <label>
+                      <span className="custom-textarea__label">
+                        Комментарий
+                        <svg width="9" height="9" aria-hidden="true">
+                          <use xlinkHref="#icon-snowflake"></use>
+                        </svg>
+                      </span>
+                      <textarea
+                        name="user-comment"
+                        minLength={MIN_COUNT_LETTERS_INPUT}
+                        maxLength={MAX_COUNT_LETTERS_INPUT}
+                        onChange={commentaryChangeHandle}
+                        value={reviewCommentary}
+                        placeholder="Поделитесь своим опытом покупки"
+                        required
+                      />
+                    </label>
+                    <div className="custom-textarea__error">
+                      Нужно добавить комментарий
+                    </div>
+                  </div>
+                )}
               </div>
               <button
                 className="btn btn--purple form-review__btn"
                 type="submit"
+                disabled={ratingStars === ''}
               >
                 Отправить отзыв
               </button>
